cb915cc7878bed21f2178f7400ac8156042ea814
Taylor Aune <taylor.aune@climate.com>
2019-04-09 14:21:35 -0700

    handling python envs correctly

--- personal/config-python.el
+++ personal/config-python.el
@@ -5,3 +5,9 @@
 (when (executable-find "ipython")
   (setq python-shell-interpreter "ipython"
         python-shell-interpreter-args "--simple-prompt -i --matplotlib=auto --pylab=auto"))
+(add-hook 'elpy-mode-hook
+          (lambda ()
+            (local-unset-key (kbd "M-TAB"))
+            (define-key elpy-mode-map (kbd "<f5>") 'elpy-company-backend)))
+(setenv "WORKON_HOME" "/Users/taylor.aune/anaconda3/envs")
+(pyvenv-mode 1)
